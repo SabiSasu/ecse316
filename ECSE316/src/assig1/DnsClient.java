81550540913ac2643b20b6bb1b2a15ffe05e84dd
Sabina <sabina.sasu@gmail.com>
2020-02-08 21:00:51 -0500

    code is ugly but works

--- ECSE316/src/assig1/DnsClient.java
+++ ECSE316/src/assig1/DnsClient.java
@@ -16,235 +16,235 @@ import java.nio.CharBuffer;
 
 public class DnsClient {
 
-  public static void main(String[] args) {
-    //java DnsClient [-t timeout] [-r max-retries] [-p port] [-mx|-ns] @server name 
+	public static void main(String[] args) {
+		//java DnsClient [-t timeout] [-r max-retries] [-p port] [-mx|-ns] @server name 
 
-    //create query, only valid if @ character is not missing for mandatory fields
-    Query q = new Query();
-    if(((args[args.length - 2]).charAt(0) == '@')) {
-      q = new Query(args);
-    }
-    else {
-      System.out.println("ERROR \t Incorrect input syntax: '@' character missing or misplaced \t ERROR");
-      System.exit(0);
-    }
+		//create query, only valid if @ character is not missing for mandatory fields
+		Query q = new Query();
+		if(((args[args.length - 2]).charAt(0) == '@')) {
+			try {
+				q = new Query(args);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				System.exit(0);
+			}
+		}
+		else {
+			System.out.println("ERROR \t Incorrect input syntax: '@' character missing or misplaced \t ERROR");
+			System.exit(0);
+		}
 
-    System.out.println("DnsClient sending request for " + q.getName());
-    System.out.println("Server: " + q.getServer()); 
-    System.out.println("Request type: " + q.getQuery().toString());
-
-
-    int retries = q.getMaxRetries();
-    long startTime = new Date().getTime();
-
-    //		while (retries > 0) { //TODO: find a way to retries-- whenever timeout occurs
-    try {
-      DatagramSocket clientSocket = new DatagramSocket(); //initialize socket
-      //set timeout, might be too short to get a reply for default value
-      //      clientSocket.setSoTimeout(q.getTimeout()); //TODO: FIX THIS TIMEOUT PROBLEM
-
-      try {
-        //get DNS server raw ip address
-        byte[] adress = q.getByteServer();
-        InetAddress IPAddress = InetAddress.getByAddress(adress);
-        System.out.println("IP address" + IPAddress.toString());
-
-        byte[] sendData = q.generateQuery(); //data to send
-
-        //printing query in hexadecimals for testing
-        for(int i =0; i < sendData.length;i++) {
-          System.out.println(String.format("%02x", sendData[i]));
-        }
-        System.out.println("packet ready");
-        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, q.getPort());
-        System.out.println("sending");
-        //IO Exception, send packet
-        clientSocket.send(sendPacket);
-
-        System.out.println("receiving");
-        //receive data
-        byte[] receiveData = new byte[1024]; //data to send
-        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-        System.out.println("receiving");
-        clientSocket.receive(receivePacket);
+		System.out.println("DnsClient sending request for " + q.getName());
+		System.out.println("Server: " + q.getServer()); 
+		System.out.println("Request type: " + q.getQuery().toString());
 
 
+		int retries = 0;
 
-        /* test code from the main.java file, also doesnt work lol
-                byte[] inBuf = new byte[8192];
-                ByteArrayInputStream inBufArray = new ByteArrayInputStream(inBuf);
-                DataInputStream input = new DataInputStream(inBufArray);
-                DatagramPacket response = new DatagramPacket(inBuf, inBuf.length);
+		while (retries < q.getMaxRetries()) {
+			long startTime = new Date().getTime();
 
-                clientSocket.receive(response);
-         */
+			try {
+				DatagramSocket clientSocket = new DatagramSocket(); //initialize socket
+				//set timeout, might be too short to get a reply for default value
+				clientSocket.setSoTimeout(q.getTimeout()*1000); //TODO: FIX THIS TIMEOUT PROBLEM
 
-        System.out.println("received, treating info");
-        long endTime = new Date().getTime();
-        long totalTime = endTime - startTime;
+				try {
+					//get DNS server raw ip address
+					byte[] adress = q.getByteServer();
+					InetAddress IPAddress = InetAddress.getByAddress(adress);
+					byte[] sendData = q.generateQuery(); //data to send
 
-        System.out.println("Response received after " + totalTime + " seconds (" + retries + " retries)");
+					//printing query in hexadecimals for testing
+					/*for(int i =0; i < sendData.length;i++) {
+						System.out.println(String.format("%02x", sendData[i]));
+					}*/
 
-        //format received information
-        String modifiedSentence = new String(receivePacket.getData());
-        System.out.println(modifiedSentence);
-        ByteBuffer b = ByteBuffer.allocateDirect(1024);
-        b.put(receivePacket.getData());
-        b.flip();
+					//send packet
+					DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, q.getPort());
+					clientSocket.send(sendPacket);
+					//receive data
+					byte[] receiveData = new byte[1024]; //data to send
+					DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+					clientSocket.receive(receivePacket);//throws timeout if times out
 
-        //process response
-        //TODO: check id to make sure its the same and find a better way to generate error!
+					long endTime = new Date().getTime();
+					long totalTime = endTime - startTime;
 
-        int respID = b.get(0) + b.get(1); //gets the ID of response
+					System.out.println("Response received after " + totalTime + " seconds (" + retries + " retries)");
 
-        if (q.getRequestID() != respID) { //TODO: DEAL WITH ERROR compare response and query IDs
-          System.out.println("Error! ID's dont match");
-        }
+					//format received information
+					ByteBuffer b = ByteBuffer.allocateDirect(1024);
+					b.put(receivePacket.getData());
+					b.flip();
 
-        int anscount = b.get(6) + b.get(7); //number of records in Answer
-        if (anscount > 0) {
+					//process response
+					int respID = b.get(0) + b.get(1); //gets the ID of response
 
-          System.out.println("***Answer Section (" + anscount + " records)***");
-       //   helper(b);
-          int offset = findEnd(b, 12); //gives us length in bytes of QNAME and NAME
-          int type = b.get(11 + offset + 4 + 2 + 1) + b.get(11 + offset + 4 + 2 + 2); //get TYPE from ANSWER
-          int theclass = b.get(11 + offset + 4 + 2 + 3) + b.get(11 + offset + 4 + 2 + 4); //get CLASS from ANSWER
+					if (q.getRequestID() != respID) { //TODO: DEAL WITH ERROR compare response and query IDs
+						System.out.println("ERROR \t Unexpected response: ID in reply does not match query ID");
+						System.exit(0);
+					}
 
-          if (theclass != 0x0001) {//error
-            //TODO: error cuz it should be 0x0001  
-          }
+					int anscount = b.get(6) + b.get(7); //number of records in Answer
+					if (anscount > 0) {
 
-          if (type == 0x0001) { //A type query
-            //IP <tab> [ip address] <tab> [seconds can cache] <tab> [auth | nonauth]
-            int ip1 = b.get(11 + offset + 4 + 2 + 11);
-            int ip2 = b.get(11 + offset + 4 + 2 + 12);
-            int ip3 = b.get(11 + offset + 4 + 2 + 13);
-            int ip4 = b.get(11 + offset + 4 + 2 + 14); //4 components of ip address
-            int ttl = b.get(11 + offset + 4 + 2 + 5) + b.get(11 + offset + 4 + 2 + 6) + b.get(11 + offset + 4 + 2 + 7) + b.get(11 + offset + 4 + 2 + 8); //get TTL from ANSWER
-            int temp = b.get(2);   
-            //mask with 000001000 to obtain AA from ANSWER
-            int mask = 0b000001000; //mask
-            if ((temp & mask) == 8) { //is authority
-              System.out.println("IP \t" + ip1 + "." + ip2 + "." + ip3 + "." + ip4 + "\t" + ttl + "\t auth");
-            }
-            else { //not authority
-              System.out.println("IP \t" + ip1 + "." + ip2 + "." + ip3 + "." + ip4 + "\t" + ttl + "\t nonauth");
-            }                   
-          }
-          else if (type == 0x0002) {//ns
-            //NS <tab> [alias] <tab> [seconds can cache] <tab> [auth | nonauth]
-            int ttl = b.get(11 + offset + 4 + offset + 5) + b.get(11 + offset + 4 + offset + 6) + b.get(11 + offset + 4 + 2 + 7) + b.get(11 + offset + 4 + 2 + 8); //get TTL from ANSWER
-            int temp = b.get(2);   
-            //mask with 000001000 to obtain AA from ANSWER
-            int mask = 0b000001000; //mask
-            String alias = readRData(b, 11 + offset + 4 + offset + 11);
+						System.out.println("***Answer Section (" + anscount + " records)***");
+						//   helper(b);
+						int offset = findEnd(b, 12); //gives us length in bytes of QNAME and NAME
+						int type = b.get(11 + offset + 4 + 2 + 1) + b.get(11 + offset + 4 + 2 + 2); //get TYPE from ANSWER
+						int theclass = b.get(11 + offset + 4 + 2 + 3) + b.get(11 + offset + 4 + 2 + 4); //get CLASS from ANSWER
 
-            if ((temp & mask) == 8) { //is auth
-              System.out.println("NS \t" + alias + "\t" + ttl + "\t auth");
-            }
-            else { //not auth
-              System.out.println("NS \t" + alias + "\t" + ttl + "\t nonauth");
-            }
-          }
-          else if (type == 0x000f) {//mx
-            //MX <tab> [alias] <tab> [pref] <tab> [seconds can cache] <tab> [auth | nonauth]
-            int pref = b.get(11 + offset + 4 + offset + 11) + b.get(11 + offset + 4 + offset + 12); //get PREFERENCE
-            String alias = readRData(b, 11 + offset + 4 + offset + 13); //get EXCHANGE
-            int ttl = b.get(11 + offset + 4 + offset + 5) + b.get(11 + offset + 4 + offset + 6) + b.get(11 + offset + 4 + 2 + 7) + b.get(11 + offset + 4 + 2 + 8); 
-            int temp = b.get(2);   
-            //mask with 000001000 to obtain AA from ANSWER
-            int mask = 0b000001000; //mask
+						if (theclass != 0x0001) {//error
+							System.out.println("ERROR \t Unexpected response: Response is not of internet class");
+							System.exit(0);
+						}
 
-            if ((temp & mask) == 8) { //is auth
-              System.out.println("MX \t" + alias + "\t" + pref + "\t" + ttl + "\t auth");
-            }
-            else { //not auth
-              System.out.println("MX \t" + alias + "\t" + pref + "\t" + ttl + "\t nonauth");
-            }
-          }
-          else if (type == 0x0005) {//cname
-            //CNAME <tab> [alias] <tab> [seconds can cache] <tab> [auth | nonauth]
-            int ttl = b.get(11 + offset + 4 + offset + 5) + b.get(11 + offset + 4 + offset + 6) + b.get(11 + offset + 4 + 2 + 7) + b.get(11 + offset + 4 + 2 + 8); 
-            String alias = readRData(b, 11 + offset + 4 + offset + 11); //TODO: im not sure if alias is formatted the same way for cname as it is for ns
-            int temp = b.get(2);   
-            //mask with 000001000 to obtain AA from ANSWER
-            int mask = 0b000001000; //mask
+						if (type == 0x0001) { //A type query
+							//IP <tab> [ip address] <tab> [seconds can cache] <tab> [auth | nonauth]
+							int ip1 = byteToInt(b.get(11 + offset + 4 + 2 + 11));
+							int ip2 = byteToInt(b.get(11 + offset + 4 + 2 + 12));
+							int ip3 = byteToInt(b.get(11 + offset + 4 + 2 + 13));
+							int ip4 = byteToInt(b.get(11 + offset + 4 + 2 + 14));
+							int ttl = byteToInt((byte) (b.get(11 + offset + 4 + 2 + 5) + b.get(11 + offset + 4 + 2 + 6) + b.get(11 + offset + 4 + 2 + 7) + b.get(11 + offset + 4 + 2 + 8)));
+							int temp = b.get(2);   
+							//mask with 000001000 to obtain AA from ANSWER
+							int mask = 0b000001000; //mask
+							if ((temp & mask) == 8) { //is authority
+								System.out.println("IP \t" + ip1 + "." + ip2 + "." + ip3 + "." + ip4 + "\t" + ttl + "\t auth");
+							}
+							else { //not authority
+								System.out.println("IP \t" + ip1 + "." + ip2 + "." + ip3 + "." + ip4 + "\t" + ttl + "\t nonauth");
+							}                   
+						}
+						else if (type == 0x0002) {//ns
+							//NS <tab> [alias] <tab> [seconds can cache] <tab> [auth | nonauth]
+							int ttl = byteToInt((byte)(b.get(11 + offset + 4 + offset + 5) + b.get(11 + offset + 4 + offset + 6) + b.get(11 + offset + 4 + 2 + 7) + b.get(11 + offset + 4 + 2 + 8))); //get TTL from ANSWER
+							int temp = b.get(2);   
+							//mask with 000001000 to obtain AA from ANSWER
+							int mask = 0b000001000; //mask
+							String alias = readRData(b, 11 + offset + 4 + offset + 11);
 
-            if ((temp & mask) == 8) { //is auth
-              System.out.println("CNAME \t" + alias + "\t" + ttl + "\t auth");
-            }
-            else { //not auth
-              System.out.println("CNAME \t" + alias + "\t" + ttl + "\t nonauth");
-            }
-          }
-          else {
-            //TODO: error if its none of the types above
-          }
-        }
+							if ((temp & mask) == 8) { //is auth
+								System.out.println("NS \t" + alias + "\t" + ttl + "\t auth");
+							}
+							else { //not auth
+								System.out.println("NS \t" + alias + "\t" + ttl + "\t nonauth");
+							}
+						}
+						else if (type == 0x000f) {//mx
+							//MX <tab> [alias] <tab> [pref] <tab> [seconds can cache] <tab> [auth | nonauth]
+							int pref = b.get(11 + offset + 4 + offset + 11) + b.get(11 + offset + 4 + offset + 12); //get PREFERENCE
+							String alias = readRData(b, 11 + offset + 4 + offset + 13); //get EXCHANGE
+							int ttl = byteToInt((byte)(b.get(11 + offset + 4 + offset + 5) + b.get(11 + offset + 4 + offset + 6) + b.get(11 + offset + 4 + 2 + 7) + b.get(11 + offset + 4 + 2 + 8))); 
+							int temp = b.get(2);   
+							//mask with 000001000 to obtain AA from ANSWER
+							int mask = 0b000001000; //mask
 
-        int arcount = b.get(10) + b.get(11); //get ARCOUNT from HEADER
-        if (arcount != 0) { //there are things in additional
-          System.out.println("***Additional Section (" + arcount + " records)***");
-          //TODO: no idea what im supposed to print here
-        }
+							if ((temp & mask) == 8) { //is auth
+								System.out.println("MX \t" + alias + "\t" + pref + "\t" + ttl + "\t auth");
+							}
+							else { //not auth
+								System.out.println("MX \t" + alias + "\t" + pref + "\t" + ttl + "\t nonauth");
+							}
+						}
+						else if (type == 0x0005) {//cname
+							//CNAME <tab> [alias] <tab> [seconds can cache] <tab> [auth | nonauth]
+							int ttl = byteToInt((byte)(b.get(11 + offset + 4 + offset + 5) + b.get(11 + offset + 4 + offset + 6) + b.get(11 + offset + 4 + 2 + 7) + b.get(11 + offset + 4 + 2 + 8))); 
+							String alias = readRData(b, 11 + offset + 4 + offset + 11); //TODO: im not sure if alias is formatted the same way for cname as it is for ns
+							int temp = b.get(2);   
+							//mask with 000001000 to obtain AA from ANSWER
+							int mask = 0b000001000; //mask
 
-      } catch (SocketException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      } catch (UnknownHostException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      } catch (IOException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
+							if ((temp & mask) == 8) { //is auth
+								System.out.println("CNAME \t" + alias + "\t" + ttl + "\t auth");
+							}
+							else { //not auth
+								System.out.println("CNAME \t" + alias + "\t" + ttl + "\t nonauth");
+							}
+						}
+						else {
+							System.out.println("NOTFOUND");
+							System.exit(0);
+						}
+					}
 
-      } finally{
-        //closing socket no matter if we get an exception or not
-        clientSocket.close();
-      }
+					int arcount = b.get(10) + b.get(11); //get ARCOUNT from HEADER
+					if (arcount != 0) { //there are things in additional
+						System.out.println("***Additional Section (" + arcount + " records)***");
+						//TODO: no idea what im supposed to print here
+						//Sabina: me neither lol
+					}
 
-    } catch (SocketException e1) {
-      // TODO Auto-generated catch block
-      e1.printStackTrace();
-    }
-  }
-  //}
+					//we're done, so exit
+					System.exit(0);
 
-  private static int findEnd(ByteBuffer b, int start) {
-    int counter = 1;
-    int result = start;
-    int curByte = b.get(result); //this is where QNAME starts
-    while (curByte != 0) {//we havent reached end of QNAME yet
-      result++;
-      counter++;
-      curByte = b.get(result);
-    }
+				}catch (SocketTimeoutException ex) {
+					retries++;
+					continue;
+				} catch (SocketException e) {
+					System.out.println("ERROR\tSocket Exception");
+					e.printStackTrace();
+				} catch (UnknownHostException e) {
+					System.out.println("ERROR\tIncorrect host name or IP");
+					e.printStackTrace();
+				} catch (IOException e) {
+					System.out.println("ERROR\tIO Exception");
+					e.printStackTrace();
+				} finally{
+					//closing socket no matter if we get an exception or not
+					clientSocket.close();
+				}
+			} catch (SocketException e1) {
+				System.out.println("ERROR\tException when creating socket");
+				e1.printStackTrace();
+			}
 
-    return counter;
-  }
 
-  private static String readRData(ByteBuffer b, int start) {
-    String result = "";
-    int num = b.get(start);
-    int count = start;
+		}
+		if(retries >= q.getMaxRetries())
+			System.out.println("ERROR \t Maximum number of retries "+ q.getMaxRetries() + " exceeded ");
+	}
 
-    while (num != 0) {
-      for(int i = 1; i <= num; i++) {
-        result.concat(Character.toString((char) b.get(count + i)));
-      }
-      count = count + num;
-      result.concat(".");
-      num = b.get(count);
-    }  
-    result = result.substring(0, result.length() - 2);
-    return result;
-  }
+	private static int findEnd(ByteBuffer b, int start) {
+		int counter = 1;
+		int result = start;
+		int curByte = b.get(result); //this is where QNAME starts
+		while (curByte != 0) {//we havent reached end of QNAME yet
+			result++;
+			counter++;
+			curByte = b.get(result);
+		}
 
-  private static void helper(ByteBuffer b) { //to help debug and look through byte buffer
-    int start = 0;
-    while (true) {
-      int curByte = b.get(start);
-      start++;
-    }
-  }
+		return counter;
+	}
+
+	private static String readRData(ByteBuffer b, int start) {
+		String result = "";
+		int num = b.get(start);
+		int count = start;
+
+		while (num != 0) {
+			for(int i = 1; i <= num; i++) {
+				result.concat(Character.toString((char) b.get(count + i)));
+			}
+			count = count + num;
+			result.concat(".");
+			num = b.get(count);
+		}  
+		result = result.substring(0, result.length() - 2);
+		return result;
+	}
+
+	private static void helper(ByteBuffer b) { //to help debug and look through byte buffer
+		int start = 0;
+		while (true) {
+			int curByte = b.get(start);
+			start++;
+		}
+	}
+
+	private static int byteToInt(byte b) {
+		return Integer.parseInt(String.format("%02X", b), 16);
+	}
 }
--- ECSE316/src/assig1/Query.java
+++ ECSE316/src/assig1/Query.java
@@ -5,7 +5,7 @@ import java.nio.ByteBuffer;
 public class Query {
 
 	private int timeout = 5;
-	private int maxRetries = 3;
+	private int maxRetries = 10;
 	private int port = 53;
 	private String server;
 	private String name;
@@ -18,25 +18,31 @@ public class Query {
 		this.name = name;
 	}
 
-	Query(String[] args){
-		
-		try{
+	Query(String[] args) throws Exception{
+
+		try {
 			this.server = args[args.length-2].substring(1);
 			this.name = args[args.length-1];
-			
+
 			for(int i = 0; i < args.length-2; i++) {
 				switch(args[i].trim()) {
 				case "-t": this.timeout = Integer.parseInt(args[i+1]); i++; break;
 				case "-r": this.maxRetries = Integer.parseInt(args[i+1]); i++; break;
 				case "-p": this.port = Integer.parseInt(args[i+1]); i++; break;
-				case "-mx": this.query = queryType.MX; i++; break; 
-				case "-ns": this.query = queryType.NS; i++; break; //made sure only takes into account mx OR ns
-				default: System.out.println("Command unknown"); break;
+				case "-mx": if(this.query == queryType.A) { 
+					this.query = queryType.MX; i++; break; 
+				} else
+					throw new Exception("ERROR\tIncorrect input syntax: Cannot define query type is already defined");
+				case "-ns": if(this.query == queryType.A) { 
+					this.query = queryType.NS; i++; break; 
+				} else
+					throw new Exception("ERROR\tIncorrect input syntax: Cannot define query type is already defined");
+				default: throw new Exception("ERROR\tIncorrect input syntax: Unknown parameters");
 				}
 			}
 		}
 		catch(Exception ex) {
-			throw ex;
+			throw new Exception("ERROR\tIncorrect input syntax: Error while parsing input");
 		}
 	}
 
@@ -94,19 +100,18 @@ public class Query {
 	public void setQuery(queryType query) {
 		this.query = query;
 	}
-	
+
 	public int getRequestID() {
 		return requestID;
 	}
 
 	public byte[] getByteServer() {
-		
+
 		String[] s = this.getServer().split("\\.");
 		byte[] b = new byte[s.length];
-		
+
 		for(int i = 0; i < b.length; i++) {
 			b[i] = (byte)(Integer.parseInt(s[i]));
-			//System.out.println(b[i]);
 		}
 		return b;
 	}
@@ -117,22 +122,20 @@ public class Query {
 		byte id2 = (byte) (Math.random()*254);
 		this.requestID = id1 + id2;
 		byte[] b= {(byte)01, (byte)00, (byte)00, (byte)01, (byte)00, (byte)00, (byte)00, (byte)00, (byte)00, (byte)00};
-		
+
 		//create buffer with header
 		ByteBuffer buff = ByteBuffer.allocateDirect(1024);
 		buff.put(id1);
 		buff.put(id2);
 		buff.put(b);
-		
+
 		//adding name in bytes
 		String[] s = this.name.split("\\.");
 		for(int i = 0; i < s.length; i++) {
 			buff.put((byte)(s[i].length()));
 			//traverse string and place each char in the buffer
 			for(int j = 0; j < s[i].length(); j++) {
-				//buff.asCharBuffer().put("a"); 
 				buff.put((byte)(s[i].charAt(j)));
-				//System.out.println(s[i].charAt(j));
 			}
 		}
 		//marking end of label
@@ -143,7 +146,6 @@ public class Query {
 		case MX: buff.put((byte)(00)); buff.put((byte)(15)); break;
 		//default: throws ("Error writing query");
 		}
-		
 		//adding internet label 00 01 at the end
 		buff.put((byte)(00)); 
 		buff.put((byte)(01)); 
